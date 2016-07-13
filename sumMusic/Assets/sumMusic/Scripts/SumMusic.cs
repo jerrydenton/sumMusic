384ca3addb9a209f9de5e1a63f8dfbc673ffa74e
jerrydenton <jerry@cyberlogical.com>
2016-07-13 02:29:52 -0400

    Made new script to handle menu button image links

--- sumMusic/Assets/sumMusic/Scripts/SumMusic.cs
+++ sumMusic/Assets/sumMusic/Scripts/SumMusic.cs
@@ -16,6 +16,7 @@ public class SumMusic : MonoBehaviour {
 
     // Current music status
     bool musicOn;
+
     /// <summary>Public accessor for current music status</summary>
     public bool MusicOn {
         get { return musicOn; }
@@ -25,11 +26,13 @@ public class SumMusic : MonoBehaviour {
         if (keepAlive)
             // Ensure object stays alive between scenes
             DontDestroyOnLoad(gameObject);
+
         // Set default state based on startOn or PlayerPrefs.
         if (saveSettings && PlayerPrefs.HasKey("sumMusicOn")) 
             musicOn = !(PlayerPrefs.GetInt("sumMusicOn") > 0);    // Convert from int to (flipped) bool
         else
             musicOn = !startOn; // Flip default before toggle
+
         ToggleMusic(true); // Use toggle to set initial state (w/o save)
     }
 
@@ -47,22 +50,28 @@ public class SumMusic : MonoBehaviour {
     /// Allows you to Toggle without saving regardless of saveSettings value
     /// </param>
     void ToggleMusic (bool skipSave) {
+
         // Check that sprites are linked properly
         if (!checkReqs()) {
             Debug.LogError("Link references missing on <b>sumMusic</b> object. Please check assignments in editor.");
             return;
         }
+
         // Flip value of musicOn
         musicOn = !musicOn;
         Debug.Log("Music status changed to " + musicOn);
         AudioSource src = GetComponent<AudioSource>();
+
         // Play or stop music
         if (musicOn)
             src.Play();
         else
             src.Stop();
-        // Switched sprite to appropriate value
-        image.sprite = musicOn ? musicOnSprite : musicOffSprite;
+
+        // Switch sprite to appropriate value
+        if(image != null)
+            image.sprite = musicOn ? musicOnSprite : musicOffSprite;
+
         // Save status to PlayerPrefs as int if needed (1=on,0=off)
         if (saveSettings && !skipSave) {
             Debug.Log("Saving sound settings");
---
+++ sumMusic/Assets/sumMusic/Scripts/SumMusicButtonImage.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+/// <remarks>
+/// Since the controller may have been loaded in another scene this 
+/// script will link this image component to the sumMusicController 
+/// object and ensure the the proper image sprite is set.
+/// </remarks>
+[RequireComponent(typeof(Image))]
+public class SumMusicButtonImage : MonoBehaviour {
+
+    SumMusic obj;
+    Image img;
+
+    public void OnEnable() {
+        // Link controller if we don't have it already
+        if (obj == null) {
+            obj = GameObject.Find("sumMusicController").GetComponent<SumMusic>();
+            // Log error and exit if the controller is still missing
+            if (obj == null) {
+                Debug.LogError("Required game object 'sumMusicController' is missing");
+                return;
+            }
+        }
+        img = GetComponent<Image>(); // Link this game object's image to the controller
+        obj.image = img;
+        // Set appropriate sprite
+        img.sprite = obj.MusicOn ? obj.musicOnSprite : obj.musicOffSprite;
+    }
+
+}
